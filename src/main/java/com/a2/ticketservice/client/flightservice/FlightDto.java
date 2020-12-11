ad7d95577fcf425e210d9d73545499354ceefa5b
andrijana.kulezic <andrijana.kulezic@gmail.com>
2020-12-11 17:01:43 +0100

    FlightDto

--- src/main/java/com/a2/ticketservice/client/flightservice/FlightDto.java
+++ src/main/java/com/a2/ticketservice/client/flightservice/FlightDto.java
@@ -1,4 +1,52 @@
 package com.a2.ticketservice.client.flightservice;
 
+import java.math.BigDecimal;
+
 public class FlightDto {
+    private String startDest;
+    private String endDest;
+    private BigDecimal miles;
+    private BigDecimal price;
+
+    public FlightDto() {
+    }
+
+    public FlightDto(String startDest, String endDest, BigDecimal miles, BigDecimal price) {
+        this.startDest = startDest;
+        this.endDest = endDest;
+        this.miles = miles;
+        this.price = price;
+    }
+
+    public String getStartDest() {
+        return startDest;
+    }
+
+    public void setStartDest(String startDest) {
+        this.startDest = startDest;
+    }
+
+    public String getEndDest() {
+        return endDest;
+    }
+
+    public void setEndDest(String endDest) {
+        this.endDest = endDest;
+    }
+
+    public BigDecimal getMiles() {
+        return miles;
+    }
+
+    public void setMiles(BigDecimal miles) {
+        this.miles = miles;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
